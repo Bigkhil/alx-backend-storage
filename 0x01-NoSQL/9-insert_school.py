bb4a727c28b7650e22a058554417a8e75f32c96e
Bigkhil <khm40005000@outlook.com>
2024-10-24 00:41:33 +0300

    first commit

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
-
+"""
+insert document
+"""
 def insert_school(mongo_collection, **kwargs):
+    '''insert document'''
     res = mongo_collection.insert_one(kwargs)
     return res.inserted_id
