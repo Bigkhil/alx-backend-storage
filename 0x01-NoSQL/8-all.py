9fc50dd9c46a69be10ef500310d0c4746ef3542b
Bigkhil <khm40005000@outlook.com>
2024-10-24 00:35:47 +0300

    first commit

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,3 +1,7 @@
 #!/usr/bin/env python3
+"""
+this file lists all the documents in a certain collection
+"""
 def list_all(mongo_collection):
+    '''list all the documents in a certain collection'''
     return mongo_collection.find()
