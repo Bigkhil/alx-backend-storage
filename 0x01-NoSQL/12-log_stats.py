47c1c0ce90ef191ba80cdc8d4e894bec58ec1a44
Bigkhil <khm40005000@outlook.com>
2024-10-24 17:11:30 +0300

    first commit

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -7,7 +7,7 @@ def main():
     '''main function'''
     client = MongoClient()
     logs = client.logs.nginx
-    print(f'{logs.count_documents()} logs')
+    print(f'{logs.count_documents({})} logs')
     print("Methods:")
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
     for method in methods:
@@ -19,7 +19,6 @@ def main():
         f'{logs.count_documents(
             {"method": "GET", "path": "/status"})} status check'
         )
-    return
 
 
 if __name__ == "__main__":
