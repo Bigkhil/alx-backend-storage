da6e222db38d8ea3aadff9944b3e34e78712ccfc
Bigkhil <khm40005000@outlook.com>
2024-10-24 16:07:46 +0300

    first commit

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
-
+"""
+this file updates docs
+"""
 def update_topics(mongo_collection, name, topics):
+    '''updating docs'''
     mongo_collection.update_many(
         {"name": name},
         {"$set": topics}
